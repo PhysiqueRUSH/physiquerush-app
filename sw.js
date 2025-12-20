36ec251012c8e6c59b216fbdbe24d1806a31108a
PhysiqueRUSH <physiquerush@gmail.com>
2025-12-20 17:56:07 +0100

    Update CACHE_NAME to version 1.06

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // NOM DU CACHE
-const CACHE_NAME = 'physiquerush-v1.05';
+const CACHE_NAME = 'physiquerush-v1.06';
 
 // Liste de tous les fichiers à mettre en cache pour le mode hors ligne
 const ASSETS_TO_CACHE = [
