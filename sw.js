bcb3fd6aec26c77039c1b9039c488805ef79942d
PhysiqueRUSH <physiquerush@gmail.com>
2025-12-20 01:19:10 +0100

    Add service worker for offline asset caching
    Implement service worker for caching assets and handling fetch requests.

---
+++ sw.js
@@ -0,0 +1,58 @@
+// Nom du cache. Changez 'v1' en 'v2', 'v3'... pour forcer la mise à jour chez les utilisateurs.
+const CACHE_NAME = 'physiquerush-v1';
+
+// Liste de tous les fichiers à mettre en cache pour le mode hors ligne
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json',
+    './physiquerush.png',
+    './MIAH.png',
+    './record.png',
+    './fury.png',
+    // Images Personnages
+    './evo1.png', './evo2.png', './evo3.png', './evo4.png', './evo5.png', './evo6.png',
+    './evo7.png', './evo8.png', './evo9.png', './evo10.png', './evo11.png', './evo12.png',
+    // Images Boss Entraînement
+    './btb1.png', './btb2.png', './btb3.png', './btb4.png',
+    // Images Boss Trophées
+    './boss1.png', './boss2.png', './boss3.png', './boss4.png',
+    // Librairie externe (graphique)
+    'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+// Installation du Service Worker (mise en cache initiale)
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Caching assets');
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+// Activation (nettoyage des vieux caches si le nom a changé)
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+// Interception des requêtes réseau (sert le cache si disponible)
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Si c'est dans le cache, on le sert, sinon on va sur le réseau
+                return response || fetch(event.request);
+            })
+    );
+});
